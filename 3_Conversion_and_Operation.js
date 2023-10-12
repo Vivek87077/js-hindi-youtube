374931d68354d27fd875a5c5461fcbf10530dd92
Vivek Shrama <vsharma87077@gmail.com>
2023-10-12 22:19:49 +0530

    Conversion of datatypes

---
+++ 3_Conversion_and_Operation.js
@@ -0,0 +1,34 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+//  let score = undefined
+let score = "Vivek"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Vivek"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/*
+Notes:--
+   "33"    => 33
+   "33abc" => Nan
+   "true"  => 1;   false => 0
+    1      => true;   0  => false
+   "Vivek" => true
+*/
+
