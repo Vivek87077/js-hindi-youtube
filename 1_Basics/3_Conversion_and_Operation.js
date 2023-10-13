ffa8b7078aec2127e510240efbd2f70f459c9c5f
Vivek87077 <144247877+Vivek87077@users.noreply.github.com>
2023-10-13 20:21:48 +0530

    Done

---
+++ 1_Basics/3_Conversion_and_Operation.js
@@ -0,0 +1,73 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+//  let score = undefined
+let score = "Vivek"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Vivek"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/*
+Notes:--
+   "33"    => 33
+   "33abc" => Nan
+   "true"  => 1;   false => 0
+    1      => true;   0  => false
+   "Vivek" => true
+*/
+
+// *************************** Operations ******************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Vivek"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3 
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1)
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+// let gameCounters = 100
+// gameCounters++ ;
+// console.log(gameCounters);
+
+// links to study more about operations
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
