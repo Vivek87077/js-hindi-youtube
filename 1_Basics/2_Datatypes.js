f1b55d0fd1fcc3c80ae01618765f283835f7929e
Vivek87077 <144247877+Vivek87077@users.noreply.github.com>
2023-10-13 20:20:34 +0530

    Done

---
+++ 1_Basics/2_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not brwser
+
+console.log("Vivek Sharma");
+
+let name = "Vivek"
+let age = 18
+let isLoggedIn = false
+/*
+Primitive data type:--
+   number => 2 to power 53
+   bigint
+   string => ""
+   boolean => true/false
+   null => standalone value(representation of empty value)
+   undefined => declared but value is not assign
+   symbol => to find the uniqueness 
+*/
+/*
+Object:-
+
+*/
+
+console.log("vivek is type of: "+typeof "Vivek") //string
+console.log("Null is type of: "+ typeof null)   //object
+console.log("undefined is type of: "+ typeof undefined) //undefined
