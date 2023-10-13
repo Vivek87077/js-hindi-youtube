0d2d0d43085707d3f2c50547e1ea008f04567010
Vivek Shrama <vsharma87077@gmail.com>
2023-10-13 19:08:52 +0530

    Basics of java script

--- 3_Conversion_and_Operation.js
+++ 3_Conversion_and_Operation.js
@@ -32,3 +32,43 @@ Notes:--
    "Vivek" => true
 */
 
+// *************************** Operations ******************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Vivek"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3 
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1)
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+// let gameCounters = 100
+// gameCounters++ ;
+// console.log(gameCounters);
+
+// links to study more about operations
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
