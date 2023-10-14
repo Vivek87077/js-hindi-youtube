efb0c9553d2f146e42a7b629b3cdc68fe077dd48
Vivek Shrama <vsharma87077@gmail.com>
2023-10-14 10:54:55 +0530

    Datatype summary

---
+++ 1_Basics/4_Datatypes_summary.js
@@ -0,0 +1,40 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id  = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 34576739789239048n
+
+
+// Reference (Non-Primitive)
+
+// types : Array, Objects, Functions,
+
+// const heros = ["shaktiman", "naagraj", "doga"]
+
+// let myObj = {
+//     name:"Vivek",
+//     age: 19,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+// console.log(typeof bigNumber)
+
+
+// Link to know further more about typeof:---
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
