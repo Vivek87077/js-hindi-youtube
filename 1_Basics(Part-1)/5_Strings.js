51ec6cf45946dca7a5678895c09708df1bf423f1
Vivek87077 <144247877+Vivek87077@users.noreply.github.com>
2024-01-06 21:49:32 +0530

    rename the folder

---
+++ 1_Basics(Part-1)/5_Strings.js
@@ -0,0 +1,33 @@
+const name = "vivek"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// Decleration of the Strings
+const gameName = new String('vivek-sharma');
+
+// Access keys of the strings
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('e'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-11, 4);
+// console.log(anotherString);
+
+// const newStringOne = "   Vivek   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/vivek%20sharma"
+// console.log(url.replace('%20' , '-'));
+// console.log(url.includes('vivek'));
+// console.log(url.includes('sundar'));
