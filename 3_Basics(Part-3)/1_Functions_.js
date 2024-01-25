59d3e786c1fb48cf796d54a2b8767df524677fa8
Vivek Shrama <vsharma87077@gmail.com>
2024-01-26 00:22:35 +0530

    Functions

--- 3_Basics(Part-3)/1_Functions_.js
+++ 3_Basics(Part-3)/1_Functions_.js
@@ -7,7 +7,7 @@ function sayMyName(){
   console.log("H");
 }
 
-// sayMyName()
+// sayMyName() 
 
 // function addTwoNumbers(number1, number2){
 
@@ -18,7 +18,7 @@ function addTwoNumbers(number1, number2){
 
   // let result = number1 + number2
   // return result
-  return number1 + number2
+  return number1 + number2               
 }
 
 const result = addTwoNumbers(3, 5)
